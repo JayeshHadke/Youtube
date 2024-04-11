16cd59a984cc6a2d781880447c68861859855df0
Jayesh Hadke <jayeshhadkejrh@gmail.com>
2024-04-11 23:30:19 +0530

    Added Register Controller and Bug Fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,53 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloud } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    const {fullName, email,userName, password} = res.body;
-    console.log(fullName, email, userName, password);
-    res.status(200).json({ message: "ok" });
+    // get user details from frontend
+    const { fullName, email, userName, password } = req.body;
+    // validation - not empty
+    if ([
+        fullName,
+        email,
+        userName,
+        password
+    ].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+    // check if user already exists: username, email
+    var existedUser = User.findOne({ $or: [{ email }, { userName }] });
+
+    if (existedUser) {
+        throw new ApiError(400, "User already exists");
+    }
+    // check for images, check for avatar
+    const avatarLocalPath = req.files.avatar ? req.files.avatar[0].path : null;
+    const coverLocalPath = req.files.cover ? req.files.cover[0].path : null;
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required");
+    }
+    // upload them to cloudinary, avatar
+    const avatar = await uploadOnCloud(avatarLocalPath);
+    const cover = coverLocalPath ? await uploadOnCloud(coverLocalPath) : null;
+    // create user object - create entry in db
+    var user = await User.create({
+        fullName,
+        email,
+        userName,
+        password,
+        avatar: avatar.url,
+        coverImage: cover ? cover.url : null
+    });
+    //  check for user creation and remove password and refresh token field from response
+    var createdUser = User.findById({ _id: user._id }).select("-password -refreshToken");
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong, please try again later");
+    }
+    // return res
+    res.status(201).json(new ApiResponse(200, createdUser, "User created successfully"));
 });
 
 export { registerUser };
